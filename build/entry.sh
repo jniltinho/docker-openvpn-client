efd303f597ea55e86c23788d59ec6a5fe28bd2ae
Nilton Oliveira <jniltinho@gmail.com>
2023-12-13 10:30:55 -0300

    Update entry.sh

--- build/entry.sh
+++ build/entry.sh
@@ -33,6 +33,7 @@ echo "using openvpn configuration file: $config_file"
 openvpn_args=(
     "--config" "$config_file"
     "--cd" "/config"
+    "--daemon"
 )
 
 if is_enabled "$KILL_SWITCH"; then
@@ -44,7 +45,7 @@ if [[ $AUTH_SECRET ]]; then
     openvpn_args+=("--auth-user-pass" "/run/secrets/$AUTH_SECRET")
 fi
 
-openvpn "${openvpn_args[@]}" --daemon
+openvpn "${openvpn_args[@]}" &
 openvpn_pid=$!
 
 trap cleanup TERM
